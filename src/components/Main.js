1d16b9f39a0f80ee3fd1045ca037ba271f3e94f5
jonslaw2000 <johnslaw2000@gmail.com>
2022-08-17 07:13:20 +0100

    Todo app

---
+++ src/components/Main.js
@@ -0,0 +1,50 @@
+import React from "react";
+import CreateTask from "./CreateTask";
+import TaskItem from "./TaskItem";
+import TaskList from "./TaskList";
+
+const tasks = [];
+ export default class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: tasks,
+        };
+    }
+    createTask = (task) => {
+        if (task.trim() === "") {
+   alert("Task cant be empty");
+return;
+}
+tasks.push({ task, isCompleted: false });
+this.setState({ tasks: tasks });
+      };
+
+toggleTask = (taskId) => {
+const taskItem = tasks[taskId];
+TaskItem.isCompleted = !taskItem.isCompleted;
+this.setState({ tasks: tasks});
+};
+
+      deleteTask = (taskId) => {
+        tasks.splice(taskId, 1);
+        this.setState({ tasks: tasks});
+      };
+ editTask = (taskId, task) => {
+        const taskItem = tasks[taskId];
+        taskItem.task = task;
+        this.setState({ tasks: tasks });
+};
+render () { 
+        return ( 
+        <div className="main">
+            <h1>Todos</h1>
+        <div className="content">
+        <CreateTask createTask={this.createTask}/>
+        <br/> 
+        <TaskList tasks={this.state.tasks} deleteTask={this.deleteTask}  editTask={this.editTask}  toggleTask={this.toggleTask} />
+        </div>
+</div>
+     ); 
+  }
+}
